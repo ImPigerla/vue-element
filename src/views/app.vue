249cbbea9317dfa7c032e86a8889e891d986e512
Spy <wujianjie@wujianjiedeMacBook-Pro.local>
2018-10-09 16:38:09 +0800

    first commot

---
+++ src/views/app.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+        <p class="text test-p">vue app , nginx ok1111</p>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'app'
+    }
+</script>
+
+<style lang="scss">
+    .test-p {
+        display : flex;
+    }
+</style>
